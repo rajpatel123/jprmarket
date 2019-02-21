f305d4767017a1fbaeea1b4040c25fdf35b503cd
Kishan <kishangupta21995@gmail.com>
2019-02-21 04:13:44 -0800

    add login page ,signup

--- app/src/main/java/com/example/kishan/grofers/LoginActivity.java
+++ app/src/main/java/com/example/kishan/grofers/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.example.kishan.grofers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+   private TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+         signup=findViewById(R.id.register_tv);
+
+         signup.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent =new Intent(LoginActivity.this,SignupActivity.class);
+                 startActivity(intent);
+             }
+         });
+
     }
 }
