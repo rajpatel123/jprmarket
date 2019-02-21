ce5008cb140a0cd8fc755e65ccfd1f5f6aeb8e5b
shubham <skingdwivedi@yahoo.in>
2019-02-21 19:18:37 +0530

    Re Designe loggin window According to groffers

--- app/src/main/java/com/example/kishan/grofers/LoginActivity.java
+++ app/src/main/java/com/example/kishan/grofers/LoginActivity.java
@@ -2,12 +2,30 @@ package com.example.kishan.grofers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+    }
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
