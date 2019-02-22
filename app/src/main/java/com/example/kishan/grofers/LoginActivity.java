a2716574facfd061e7252de67a5327772895784e
Kishan <kishangupta21995@gmail.com>
2019-02-21 19:46:36 -0800

    add registration validation

--- app/src/main/java/com/example/kishan/grofers/LoginActivity.java
+++ app/src/main/java/com/example/kishan/grofers/LoginActivity.java
@@ -3,24 +3,83 @@ package com.example.kishan.grofers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
    private TextView signup;
+   private EditText email,password;
+   private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
          signup=findViewById(R.id.register_tv);
+         email=findViewById(R.id.edit_email);
+        password=findViewById(R.id.edit_Passwword);
+        login=findViewById(R.id.loginButton);
 
-         signup.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean check=true;
+
+                String gmaill=email.getText().toString();
+                String passworddd=password.getText().toString();
+
+
+
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(gmaill).matches())
+                {
+                    email.setError("Field is empty");
+                    check=false;
+                }
+
+
+                if(passworddd.length()<10)
+                {
+                    password.setError("enter more than 10 charater");
+                    check=false;
+                }
+                if (check==true)
+                {
+                    Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(LoginActivity.this,Main2Activity.class);
+
+                    intent.putExtra("USERNAME","gmail");
+                    intent.putExtra("PASSWORD","password");
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                 Intent intent =new Intent(LoginActivity.this,SignupActivity.class);
+                 Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
                  startActivity(intent);
-             }
-         });
 
+             }
+        });
     }
 }
--- app/src/main/java/com/example/kishan/grofers/SignupActivity.java
+++ app/src/main/java/com/example/kishan/grofers/SignupActivity.java
@@ -1,13 +1,93 @@
 package com.example.kishan.grofers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText name,mobile,country,email,pwd;
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name=findViewById(R.id.nameET);
+        mobile=findViewById(R.id.mobileET);
+        country=findViewById(R.id.countryET);
+        email=findViewById(R.id.emailET);
+        pwd=findViewById(R.id.pwdET);
+        submit=findViewById(R.id.submitBTN);
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Intent intent=new Intent(Main3Activity.this,Main2Activity.class);
+                //   startActivity(intent);
+
+
+
+                boolean check = true;
+                String namee = name.getText().toString();
+                String mobilee = mobile.getText().toString();
+                String countryy = country.getText().toString();
+                String emaill = email.getText().toString();
+                String pwdd = pwd.getText().toString();
+                if (namee.length()<10) {
+                    name.setError("Enter more than 15 character!!");
+                    check = false;
+                }
+
+                if (mobilee.isEmpty()) {
+                    mobile.setError("fields is empty");
+                    check = false;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(emaill).matches())
+                {
+                    email.setError("Enter Valid email");
+                    check = false;
+                }
+
+
+
+
+
+                if (pwdd.length()<8) {
+                    pwd.setError("enter valid password");
+                    check = false;
+                }
+
+                if (countryy.length()<4) {
+                    country.setError("enter valid country");
+                    check = false;
+
+                }
+                if (check == true) {
+
+                    Toast.makeText(SignupActivity.this, "Registration Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(SignupActivity.this,Main2Activity.class);
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(SignupActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+
+
+
+        });
+
     }
 }
