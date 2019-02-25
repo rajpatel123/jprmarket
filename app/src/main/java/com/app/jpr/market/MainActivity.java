1de61159cbef8714f1923a3bd83bf09b88d28ed4
Kishan <kishangupta21995@gmail.com>
2019-02-25 10:28:01 -0800

    show progress dialog

--- app/src/main/java/com/app/jpr/market/MainActivity.java
+++ app/src/main/java/com/app/jpr/market/MainActivity.java
@@ -1,13 +1,9 @@
 package com.app.jpr.market;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -85,6 +81,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+
+        if(id==R.id.nav_location){
+            Intent intent=new Intent(MainActivity.this,NearbyLocationActivity.class);
+            startActivity(intent);
+        }
 
        if (id == R.id.nav_login) {
            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
