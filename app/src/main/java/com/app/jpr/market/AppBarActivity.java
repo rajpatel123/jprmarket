44ae027a875c5f9aab3fa7ff5328e46950198dee
Kishan <kishangupta21995@gmail.com>
2019-02-25 23:11:49 -0800

    Registration api

---
+++ app/src/main/java/com/app/jpr/market/AppBarActivity.java
@@ -0,0 +1,42 @@
+package com.app.jpr.market;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class  AppBarActivity extends AppCompatActivity {
+    private Button catagory,search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar_main);
+
+        catagory=findViewById(R.id.catagoryBTN);
+        search=findViewById(R.id.searchBTN);
+
+        catagory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(AppBarActivity.this,CatagoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(AppBarActivity.this,SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
