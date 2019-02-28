3cd29c770418c8ac4b784829d0dad5221c914c98
Kishan <kishangupta21995@gmail.com>
2019-02-28 06:19:08 -0800

    Search view on toolbar and icon button

---
+++ app/src/main/java/com/app/jpr/market/Activities/SearchActivity.java
@@ -0,0 +1,77 @@
+package com.app.jpr.market.Activities;
+
+
+
+
+import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.SearchView;
+        import android.support.v7.widget.Toolbar;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.ListView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import com.app.jpr.market.R;
+
+public class SearchActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+    ArrayAdapter mAdapter;
+    ListView mListView;
+    TextView mEmptyView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mListView = (ListView) findViewById(R.id.list);
+        mEmptyView = (TextView) findViewById(R.id.emptyView);
+
+        mAdapter = new ArrayAdapter(SearchActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.months_array1));
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(SearchActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mListView.setEmptyView(mEmptyView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutoolbar, menu);
+
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+
+        SearchView mSearchView = (SearchView) mSearch.getActionView();
+        mSearchView.setQueryHint("Search for products");
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
