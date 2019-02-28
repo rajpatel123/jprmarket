e95ab252253bc03d83ffb43b17b4028dc37379b2
Kishan <kishangupta21995@gmail.com>
2019-02-28 06:21:42 -0800

    category api

--- app/src/main/java/com/app/jpr/market/AppBarActivity.java
+++ app/src/main/java/com/app/jpr/market/AppBarActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.app.jpr.market.R;
+
 public class  AppBarActivity extends AppCompatActivity {
     private Button catagory,search;
 
