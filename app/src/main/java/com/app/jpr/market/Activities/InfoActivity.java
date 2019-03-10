d9db27ab44673cb946a8d49c992754977bc40a2a
Kishan <kishangupta21995@gmail.com>
2019-03-10 05:25:50 -0700

    add elegent button

---
+++ app/src/main/java/com/app/jpr/market/Activities/InfoActivity.java
@@ -0,0 +1,23 @@
+package com.app.jpr.market.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.app.jpr.market.R;
+
+public class InfoActivity extends AppCompatActivity {
+private TextView info;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+//
+//        info=findViewById(R.id.infoTV);
+//        Intent intent=new Intent(InfoActivity.this,ProductActivity.class);
+//        startActivity(intent);
+
+
+    }
+}
--- app/src/main/java/com/app/jpr/market/Activities/ProductActivity.java
+++ app/src/main/java/com/app/jpr/market/Activities/ProductActivity.java
@@ -1,10 +1,13 @@
 package com.app.jpr.market.Activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.app.jpr.market.R;
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 
 import java.util.ArrayList;
 
@@ -13,13 +16,22 @@ import android.os.Bundle;
         import android.os.Handler;
         import android.support.v4.view.ViewPager;
         import android.support.v7.app.AppCompatActivity;
-        import java.util.ArrayList;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
         import java.util.Timer;
         import java.util.TimerTask;
 
         import me.relex.circleindicator.CircleIndicator;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private TextView info;
+    ElegantNumberButton btn;
+   // private ElegantNumberButton button;
+
     private static ViewPager mPager;
     private static int currentPage = 0;
     private static final Integer[] XMEN= {R.drawable.veg, R.drawable.vegetable,R.drawable.veg,R.drawable.vegetable,R.drawable.veg};
@@ -29,6 +41,41 @@ public class ProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
         init();
+
+        btn = (ElegantNumberButton) findViewById(R.id.mybutton);
+
+        // elegent button
+        btn.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num=btn.getNumber();
+                Log.e("MIN",num);
+            }
+        });
+
+
+//         button =  findViewById(R.id.button);
+//        button.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String num = button.getNumber();
+//            }
+//        });
+
+
+        info=findViewById(R.id.infoTV);
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(ProductActivity.this,InfoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
     private void init() {
         for(int i=0;i<XMEN.length;i++)
