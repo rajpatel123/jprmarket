c4ea07bda6e761fac43719a2a911dd44de88360e
Kishan <kishangupta21995@gmail.com>
2019-03-14 22:47:39 -0700

    add elegent button

--- app/src/main/java/com/app/jpr/market/Activities/MyCardActivity.java
+++ app/src/main/java/com/app/jpr/market/Activities/MyCardActivity.java
@@ -1,30 +1,51 @@
 package com.app.jpr.market.Activities;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.app.jpr.market.R;
 
 public class MyCardActivity extends AppCompatActivity {
-    private Button shopping;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_card);
+        setContentView(R.layout.activity_my_card2);
 
-        shopping=findViewById(R.id.enter_BTN);
+        if(getSupportActionBar()!=null){                                                                                  ///
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);                                                       ///
+            getSupportActionBar().setDisplayShowHomeEnabled(true);                                                       ///
 
-        shopping.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MyCardActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        }
 
+
+
+
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {                                                          //
+        int id = item.getItemId();                                                                                 //
+//        switch (id) {                                                                                               //
+//            case R.id.action_settings:                                                                               //
+//                //Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+//                Intent intent= new Intent(CategoryActivity.this, AddCard.class);
+//                startActivity(intent);//
+//                return true;
+//
+//        }
+
+        ///show back button
+        if(item.getItemId()==android.R.id.home)                                                                     ///
+        {                                                                                                           ///
+            finish();                                                                                               ///
+            return super.onOptionsItemSelected(item);                                                               ///
+        }                                                                                                          ///
+        return true;
     }
 }
