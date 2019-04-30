e3ea0f6acb4684cd086d8e33c10f4450fd33c0e0
Kishan <kishangupta21995@gmail.com>
2019-04-29 22:57:14 -0700

    Expandable listview

--- app/src/main/java/com/app/jpr/market/retrofit/RestClient.java
+++ app/src/main/java/com/app/jpr/market/retrofit/RestClient.java
@@ -22,11 +22,11 @@ public class RestClient {
    public static void registerUser(RequestBody u_name , RequestBody u_email , RequestBody u_mobile, RequestBody u_country , RequestBody u_password, Callback<RegistrationResponse> callback){
         RetrofitClient.getClient().register(u_name,u_email,u_mobile,u_country,u_password).enqueue(callback);
    }
-//
-//    //Category                             //imp
-//    public static void getCourses(Callback <List<CatagoryResponse>> callback) {
-//        RetrofitClient.getClient().getCourse().enqueue(callback);
-//    }
+
+    //Category                             //imp
+    public static void getCourses(Callback <List<CatagoryResponse>> callback) {
+        RetrofitClient.getClient().getCourse().enqueue(callback);
+    }
 
 
     //Subcatagory
